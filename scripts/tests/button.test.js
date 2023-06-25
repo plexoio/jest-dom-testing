b90ce80e72ba7e4a30eafbaa6d1cd8d0b6635612
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-25 18:03:06 +0000

    finish learn Jest DOM & functions

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,12 +1,28 @@
-const buttonClick = require('../button');
+// By specific components, MOCKS
+// beforeEach(() => {
+//     document.body.innerHTML = `<p id="my-text"></p>`;
+// });
 
+// Imports
+const buttonClick = require('../button'); // Could be named differently
+
+// By loading the whole HTML file, MOCKS
 beforeEach(() => {
-    document.body.innerHTML = `<p id="my-text"></p>`;
-})
+    let fs = require('fs');
+    let htmlContents = fs.readFileSync('index.html','utf-8');
+    document.open();
+    document.write(htmlContents);
+    document.close();
+});
 
+// Run tests, DIRECTIVES & MATCHERs
 describe('DOM tests', () => {
     test('Changing <p> content', () => {
-        buttonClick();
+        buttonClick(); // Test this function and compare
         expect(document.getElementById('my-text').innerHTML).toEqual('You clicked me!'); // Matcher function needed
     })
-})
+    
+    test('Does one h1 exist?', () => {
+        expect(document.getElementsByTagName('h1').length).toBeGreaterThanOrEqual(1); // toBe() for specific
+    })
+});
